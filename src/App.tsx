b5d97b16171831d393ba9bf7e00497f5d27a03a4
HelloHailie <alsltjsals@gmail.com>
2022-12-29 01:56:35 +0900

    input 창까지

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./components/model";
+import TodoList from "./components/TodoList";
 
-function App() {
+// let name: string;
+// let age: number | string;
+// let isStudent: boolean;
+// let hobbies: string[];
+// let role: [number, string];
+// let personName: unknown;
+
+// // let printName: Function;
+// let printName: (name: string) => void; //아무것도 반환하지 않는 함수에 사용하는 타입
+
+// // function printName(name: string) {
+// //   console.log(name);
+// // }
+
+// // printName("seonmin");
+
+// // type Person = {
+// //   name: string;
+// //   age?: number;
+// // };
+// // let person: Person = {
+// //   name: "seonmin",
+// // };
+// // let lotsOfPerson: Person[] = {};
+
+// interface Person {
+//   name: string;
+//   age?: number;
+// }
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <span className='heading'>Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList />
     </div>
   );
-}
+};
 
 export default App;
