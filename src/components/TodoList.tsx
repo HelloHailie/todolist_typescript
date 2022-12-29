a1ab2ca411ec5d2354823965f872f6697b984912
HelloHailie <alsltjsals@gmail.com>
2022-12-29 10:57:28 +0900

    feat:done

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "./styles.css";
+import { Todo } from "./model";
+import SingleTodo from "./SingleTodo";
 
-const TodoList = () => {
-  return <div>TodoList</div>;
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList = ({ todos, setTodos }: Props) => {
+  return (
+    <div className='todos'>
+      {todos.map((todo) => (
+        <SingleTodo
+          todo={todo}
+          key={todo.id}
+          todos={todos}
+          setTodos={setTodos}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
