0f5383295c42db83f829e4b0265b2057b1da3b0c
HelloHailie <alsltjsals@gmail.com>
2023-01-03 23:18:54 +0900

    feat:헤더 이름 변경

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ const App: React.FC = () => {
 
   return (
     <div className='App'>
-      <span className='heading'>Taskify</span>
+      <span className='heading'>Todo List</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
       <TodoList todos={todos} setTodos={setTodos} />
     </div>
